7fbd4446ae8560a5dbb64ddb0cc87abe308b30eb
bi-zx <647617764@qq.com>
2026-03-25 09:25:43 +0800

    feat: add scripts

---
+++ scripts/start_uran_core.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+source /SDCARD/uran_ws/install/setup.bash
+exec ros2 run uran_core uran_core_node
---
+++ scripts/start_uran_move.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+source /SDCARD/uran_ws/install/setup.bash
+exec ros2 run uran_move uran_move_node
