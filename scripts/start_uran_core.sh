28ba00363fca396c8628770dd0a9f36cd710d5a4
bi-zx <647617764@qq.com>
2026-03-30 10:41:35 +0800

    Update scripts

--- scripts/start_uran_core.sh
+++ scripts/start_uran_core.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
+source /opt/ros2/galactic/setup.bash
+source /opt/ros2/cyberdog/setup.bash
+source /opt/ros2/cyberdog/local_setup.bash
 source /SDCARD/uran_ws/install/setup.bash
 exec ros2 run uran_core uran_core_node
